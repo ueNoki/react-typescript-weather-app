4a7f7809e05bf011345b9a6c27bc21d99901b553
uenoki <barabaracrm@gmail.com>
2022-05-07 20:38:00 +0900

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
+//App.tsx
+
 import './App.css';
 import { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
-import Result from "./components/Result";
+import Results from "./components/Results";
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
 
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
   const getWeather = (e:any) => {
     e.preventDefault();
-    fetch("https://api.weatherapi.com/v1/current.json?key=056558385454427188155609220705&q=London&aqi=no").then(res => res.json().then(data => console.log(data)));
+    fetch("https://api.weatherapi.com/v1/current.json?key=056558385454427188155609220705&q=London&aqi=no").then(res => res.json().then(data => 
+      setResults({
+        country: data.location.country,
+        cityName: data.location.name,
+        temperature: data.current.temp_c,
+        conditionText: data.current.condition.text,
+        icon: data.current.condition.icon,
+    })
+    ));
   }
   return (
     <div className="App">
       <Title />
       <Form setCity={setCity} getWeather={getWeather} />
-      <Result />
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/components/Results.tsx
@@ -0,0 +1,18 @@
+// Result.tsx
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
+
+
+const Result = (props: ResultsStateType) => {
+  return (
+    <h1>気象データ{props.results.country}</h1>
+  );
+};
+
+export default Result;
